ffd6fb109bc8391d23b442f6abf911b605e5026c
Shourya Rai <shouryarai607@gmail.com>
2026-09-26 15:41:10 +0530

    Time: 7 ms (96.26%), Space: 73.2 MB (7.09%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,24 +1,100 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer, Integer> mpp=new HashMap<>();
-      int k=2, l=0, r=0, maxLen=0;
+    //     HashMap<Integer, Integer> mpp=new HashMap<>();
+    //   int k=2, l=0, r=0, maxLen=0;
 
-      while(r<fruits.length){
-        mpp.put(fruits[r], mpp.getOrDefault(fruits[r], 0)+ 1);
-        while (mpp.size()>k){
-            mpp.put(fruits[l], mpp.get(fruits[l])-1);
+    //   while(r<fruits.length){
+    //     mpp.put(fruits[r], mpp.getOrDefault(fruits[r], 0)+ 1);
+    //     while (mpp.size()>k){
+    //         mpp.put(fruits[l], mpp.get(fruits[l])-1);
         
-        if (mpp.get(fruits[l])==0){
-            mpp.remove(fruits[l]);
-        }
-            l++;
-        }
-            maxLen= Math.max(maxLen, r-l+1);
-            r++;
+    //     if (mpp.get(fruits[l])==0){
+    //         mpp.remove(fruits[l]);
+    //     }
+    //         l++;
+    //     }
+    //         maxLen= Math.max(maxLen, r-l+1);
+    //         r++;
         
-      }
+    //   }
 
-        return maxLen;
+    //     return maxLen;
+
+
+
+
+    // need hashmap because want to store frequency of duplicate fruit
+    // HashSet<Integer> set= new HashSet <>();
+
+    // int l=0, r=0, sum=0, maxsum=0;
+    // int limit=2;
+
+    // while(r<fruits.length){
+    //     if(limit != 0){
+    //         if(set.contains(fruits[r])){
+    //             sum++;
+    //             r++;
+    //         }else{
+    //         set.add(fruits[r]);
+    //         sum++;limit--;
+    //         r++;
+    //         }
+    //     }
+    //     else if(limit==0){
+    //         if(set.contains(fruits[r])){
+    //             sum++;
+    //             r++;
+    //         }else{
+    //         set.remove(fruits[l]);
+    //         set.add(fruits[r]);
+    //         l++;r++;
+    //         }
+    //     }
+    //     maxsum=Math.max(maxsum, sum);
+    // }
+    // return maxsum;
+
+    int fruit1=-1,fruit2=-1;
+    int max=0;
+    int count1=0, count2=0;
+
+    int l=0;
+    for(int r=0;r<fruits.length;r++){
+
+        if(fruits[r]==fruit1){
+            count1++;
+        }else if(fruits[r]==fruit2){
+            count2++;
+        }
+        else{
+            while(count1>0 && count2>0){
+                if(fruits[l]==fruit1){
+                    count1--;
+                }else{
+                    count2--;
+                }
+                l++;
+            }
+
+            if(count1==0){
+            fruit1= fruits[r];
+            count1++;
+        }else{
+            fruit2=fruits[r];
+            count2++;
+        }
+        }
+
+
         
+        max= Math.max(max, r-l+1);
+    }
+    return max;
+
     }
 }
+
+
+
+
+
